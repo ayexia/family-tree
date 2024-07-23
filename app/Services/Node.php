cfabf1a8c06126ff1dc293cac42cb1c7e6010772
ayexia <79761489+ayexia@users.noreply.github.com>
2024-07-23 09:08:01 +0100

    updated getter setter methods

--- app/Services/Node.php
+++ app/Services/Node.php
@@ -11,13 +11,19 @@ class Node {
     public $children = [];
     public $spouses = [];
     public $parents = [];
+    public $father_id;
+    public $mother_id;
+    public $gender;
 
-    public function __construct($id, $name, $birth_date, $death_date) {
+    public function __construct($id, $name, $birth_date, $death_date, $gender, $father_id = null, $mother_id = null) {
         $this->id = $id;
         $this->name = $name;
         //formats dates via Carbon in year-month-date format, removing timestamp. if DOB/DOD is null replaces value with arbitrary "unknown date"
         $this->birth_date = $birth_date ? Carbon::parse($birth_date)->format('Y-m-d') : 'Unknown date'; 
         $this->death_date = $death_date ? Carbon::parse($death_date)->format('Y-m-d') : 'Unknown date';
+        $this->gender = $gender;
+        $this->father_id = $father_id;
+        $this->mother_id = $mother_id;
     }
 
     public function addChild(Node $child) {
@@ -30,17 +36,22 @@ class Node {
 
     public function addParent(Node $parent) {
         $this->parents[$parent->id] = $parent;
+        if ($parent->gender === 'M') {
+            $this->father_id = $parent->id;
+        } else {
+            $this->mother_id = $parent->id;
+        }
     }
 
-    public function getChildren(){
+    public function getChildren() {
         return $this->children;
     }
 
-    public function getSpouses(){
+    public function getSpouses() {
         return $this->spouses;
     }
 
-    public function getParents(){
+    public function getParents() {
         return $this->parents;
     }
 }

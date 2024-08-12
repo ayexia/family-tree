2c3569040861a91ddc736015744ae3cb9c43783e
ayexia <79761489+ayexia@users.noreply.github.com>
2024-08-12 07:20:08 +0100

    updated about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,6 +10,10 @@
 .profile-button {
     text-decoration: none;
 }
+.footer {
+    position: absolute;
+    bottom: -150px;
+}
 </style>
 <body>
     <div class="container">
@@ -28,16 +32,21 @@
             </a>                    
         <div class="main-content">
             <div class="textbox one">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            </div>
+            Welcome to MyStory, the ultimate tool for preserving and exploring your family history. Whether you're looking to visualise your ancestry or share cherished memories, MyStory is designed to make your journey into genealogy both comprehensive and accessible.
+        <br><br> Our goal is to make family history convenient and enjoyable for everyone. We strive for accuracy in the details and simplicity in navigation, ensuring that your experience is both personalised and inclusive.
+    <br><br>At MyStory, we believe families are more than simply genetics and blood relations!</div>
             <div class="about tree">
-            <img src="{{ asset('images/about-image-1.png') }}" alt="User 1">
+            <img src="{{ asset('images/about-image-1.png') }}" alt="About 1">
             </div>
             <div class="textbox two">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            </div>
+            Features:
+        <br><br>GEDCOM File Import: Easily import your existing family history data through GEDCOM files, instantly creating a rich, interactive family tree.
+                <br><br>Visualisations: Explore your family history through various visual formats, making it easy to see connections and understand your lineage at a glance -- control the view you wish to see!
+                <br><br>Edit Family Information: Update and refine your family tree through editing information and uploading images, documenting important events as accurately as possible.
+                <br><br>Add Memories: Attach photos, stories, and other memories to individual profiles, making sure each and every family member's legacy is fully captured.
+                <br><br>PDF Book Creation: Compile your family tree into a beautifully designed PDF book, customisable for your needs. It makes the perfect gift for sharing with loved ones or even preserving as a keepsake.</div>
             <div class="about dna">
-            <img src="{{ asset('images/about-image-2.png') }}" alt="User 1">
+            <img src="{{ asset('images/about-image-2.png') }}" alt="About 2">
             </div>
         </div>
     </div>
